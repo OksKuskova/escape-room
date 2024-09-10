78193ad3018b798a41e80dd16b8890abf6e43cee
OksKuskova <oskanchik.kuskova@gmail.com>
2024-09-10 08:53:35 +0200

    StrictMode

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-// import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import { Provider } from 'react-redux';
@@ -12,9 +12,9 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
